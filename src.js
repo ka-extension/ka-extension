f9ef67c146dd9825172a3b4f7d2fc1e812b032ce
Luke Krikorian <lukekrikorian@gmail.com>
2017-11-28 13:27:07 -0500

    Added downvote button

--- src.js
+++ src.js
@@ -36,8 +36,8 @@ function newDate(date) {
     var d = new Date(date);
     return (("0"+(d.getMonth()+1)).slice(-2) + "/" + ("0" + d.getDate()).slice(-2) + "/" + d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2));
 }
-    
-    
+
+
 /*** When notifications are more than 9, it will show the real value and not 9+ anymore. ***/
 var updateNotifs = function() {
     var dropdown = document.getElementsByClassName('switchText_1lh86m9')[0].childNodes[5];
@@ -55,15 +55,16 @@ var updateNotifs = function() {
 
 /*** When viewing a program, it shows how many flags the program has. ***/
 var addFlagsToProgram = function(){
+    if(programData.scratchpad === undefined) return;
     var flag = document.getElementsByClassName('discussion-meta-controls');
     var title = document.getElementsByClassName('editTitle_swrcbw');
     var programFlags = programData.scratchpad.flags;
 
     if(flag.length < 1 && title.length < 1){ return; }
-    
+
     var flagString = ' • ' + programData.scratchpad.flags.length;
     flag[0].childNodes[2].innerHTML += flagString;
-    
+
     // Hover over flag button to show flag reasons.
     var flagBtn = document.getElementsByClassName("link_1uvuyao-o_O-computing_77ub1h")[0];
 
@@ -74,16 +75,19 @@ var addFlagsToProgram = function(){
         });
         flagBtn.title = reasons;
     };
-    clearInterval(addFlags);    
+    clearInterval(addFlags);
 };
 
 /*** When viewing a program, it shows when it was created and last updated. ***/
 var showProgramDates = function() {
-    var date = document.getElementsByClassName("link_1uvuyao-o_O-computing_1nblrap author-nickname profile-programs")[0];
+
+    var dateElm = document.getElementsByClassName("link_1uvuyao-o_O-computing_1nblrap")[0];
+    if(dateElm === undefined || dateElm.nextElementSibling === null) return;
+    console.log(dateElm)
     var createdDate = newDate(programData.scratchpad.created);
     var updatedDate = newDate(programData.scratchpad.date);
 
-    date.nextElementSibling.innerHTML = "<br>Created: " + createdDate + "<br>Last updated: " + updatedDate;
+    dateElm.nextElementSibling.innerHTML = "<br>Created: " + createdDate + "<br>Last updated: " + updatedDate;
     clearInterval(getDates);
 };
 
@@ -96,11 +100,9 @@ var getProfileData = function(){
     var tableBody = table.childNodes[0];
     var kaid = userInfo.kaid;
     var dateJoined = userInfo.dateJoined;/*.substring(0,10);*/
-        
-    
     
     var numVotes = 0;
-    var numSpinoffs = 0; 
+    var numSpinoffs = 0;
     var numPrograms = userPrograms.scratchpads.length;
     for(var i = 0; i < numPrograms; i++){
         var scratchpad = userPrograms.scratchpads[i];
@@ -120,10 +122,9 @@ var getProfileData = function(){
 /*** When viewing the hotlist or a projects list, flag counts will be added next to spinoffs. ***/
 var showProgramsFlags = function() {
     var programLinks = document.getElementsByClassName("link_1uvuyao-o_O-noUnderline_4133r1"), i = 0;
-    if (programLinks.length < 2) { 
+    if (programLinks.length < 2) {
         return;
     };
-
     var handleResponse = function(a) {
         ids.push(+programLinks[counter].href.split("/")[5]);
         objs.push(a);
@@ -145,18 +146,44 @@ var showProgramsFlags = function() {
     console.log("Done");
     clearInterval(programFlags);
 };
+var addDownvoteButton = function(){
+  var buttons = document.getElementsByClassName("link_1uvuyao-o_O-computing_1w8n1i8")[1]["parentNode"];//buttons_vponqv
+  if(!buttons || !programData) return;
+  window.programData = programData;
+  var text = "Vote Down • " + programData.scratchpad.sumVotesIncremented;
+  var span = document.createElement('span');
+  span.style = "display: inline-block; position: relative;"
+  span.innerHTML = '<a aria-disabled="false" role="button" href="javascript:void(0)" class="link_1uvuyao-o_O-computing_1w8n1i8"><span>' + text + '</span></a>'
+  span.onclick = function(){
+    var getSession = () => /fkey=(.*?);/ig.exec(document.cookie)[1];
+    var key = window.programData.scratchpad.key;
+    var req = new XMLHttpRequest();
+    req.open("POST", "https://www.khanacademy.org/api/internal/discussions/voteentity");
+    req.setRequestHeader('x-ka-fkey', getSession());
+    req.setRequestHeader('Content-type', "application/x-www-form-urlencoded");
+    req.send("entity_key=" + key + "&vote_type=-1")
+    req.addEventListener('load', function(){
+      var b1 = document.getElementsByClassName("link_1uvuyao-o_O-computing_1w8n1i8")[1];
+      var b2 = document.getElementsByClassName("link_1uvuyao-o_O-computing_1w8n1i8")[2];
+      b1.innerHTML = "Vote Up • " + (programData.scratchpad.sumVotesIncremented - 1);
+      b2.innerHTML = "Vote Down • " + (programData.scratchpad.sumVotesIncremented - 1);
+
+    });
+  };
+  buttons.appendChild(span);
+  clearInterval(addDownvote);
+};
 
 if (window.location.host === 'www.khanacademy.org') {
     var notifications = setInterval(updateNotifs, 250);
     if (url[3] === 'computer-programming') {
         var addFlags = setInterval(addFlagsToProgram,250);
         var getDates = setInterval(showProgramDates, 250);
+        var addDownvote = setInterval(addDownvoteButton, 250);
     } else if (url[3] === 'profile') {
         var profileData = setInterval(getProfileData, 250);
     } else if (url[5] === 'browse') {
         var programFlags = setInterval(showProgramsFlags, 250);
+
     }
 }
-
-
-
