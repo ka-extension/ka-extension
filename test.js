36b2b677a6145277b59ed9962ebfc0a3d3f08342
Luke Krikorian <lukekrikorian@gmail.com>
2017-11-29 14:11:58 -0500

    First commit

---
+++ test.js
@@ -0,0 +1,85 @@
+// to be fair  you have to have a fairly high iq to understand rick and morty, the humour is extremely subtle and without a solid grasp of theoretical physics most jokes will go over the typical viewer's head. There's Rick's nihilistic outlook for instance, which draws heavily from (ISDjflsjf) literature.
+
+// are you really going to type that all out?
+var draw = function(){
+  //ohno there goes le framerate
+  // ok let's call get a buch of times because that's cool
+  fill(0);//look i only us one rgb praram. 10 THINGS YOU DID NOT KNOW ABOUT PROGRAMMING ON perido bnecause pjs is the onyl progaming language ever
+
+  rect(10,100,100,100);
+  // wow that's like magic!!! i never knew you could do that
+  // let's cache this image of a circle
+
+  ellipse(50, 50, 100, 100);
+  var g = get(0, 0, 100, 100);
+  g();
+  //WATC H THIS COOL HACK
+  //why is that purple
+
+  println(this.__framerate);//////////////////////////////////hacks\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// wow you must be like a hacker or something
+// wach how i can change this image thumbnail!!!!!!
+heck you are a haker
+u
+u // no hackers allowd on kahan aacademy!!!!
+HOW DO LOCATION HACLlllll??????????????
+how do i change my bio
+
+??????????????????????????????????????????????????????????????????????
+
+  // wow now e have an image
+};
+// I would but I don't have an account on khan aacademyâ
+//change your name on kahn academy to greek text no clickbait HACK
+// oh no thats relly sad how do you live
+//Don't worry im going to kill myself
+//;)))))))))))))))))))))))))))))))))))))))))))))))
+
+
+
+
+
+ten things you did not know about kahn academy
+1. things do not worrk
+2. you can do this ()()()()()() to stop oh nos
+3. sal kahn is !(smart)/////////////////////gotem
+2. i like kahn academy
+3. i spend fiv hours everyday on it
+4. Ethan Luis McDonough spent around 15% of his life answering questions in the computer programming section of ka hoping that one day he would be a mod
+sad stori yes
+
+5. a lot of them were just me pasting my explanation on how to embed pjs in webpages tbh
+////////////////////understandable have a nice everyday
+Do you know how fkeys worrk
+I want to make a KA bot but I don't know how often the fkey changes'
+Every time someone mentions jett burns the bot says deez nut
+hAKER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+what do you have in mind
+wOAH
+that would require some serious http requests a
+very expensive
+unless you want to whitelist certain programs or users
+
+
+Very literature what
+maybe just have it only on one program
+Idk
+But how do you get a users fkey if you know their username and password
+
+i havn't tested THIS, but you might be able to log in and store the request's cookies in some sort of cookie manager object
+just a though
+or you could use oauth
+But fkeys change everydayBRO
+#
+exactly
+which iss why you should fucking kill yourself
+i agree
+which is why you could use oauth to get a new oauth token every time you want to make authenitcated api requests
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::ok nice""""""""""""""""""""""""
+
+
+
+I hvae to go KNOW
+
+going to okhang myself bye
