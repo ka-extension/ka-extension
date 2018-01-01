30a340e6061941af9bf9eae033214e5c1260b2f5
Ethan Luis McDonough <elmcdonough@comcast.net>
2017-12-31 23:14:22 -0600

    Add toggle dark theme button

--- inject/src.js
+++ inject/src.js
@@ -20,7 +20,10 @@ var aceThemes = {
 
 /* Ace Monokai theme.  Taken from https://github.com/ajaxorg/ace/blob/master/lib/ace/theme/monokai.css */
 aceThemes.addTheme("monokai", ".ace-monokai .ace_gutter {background: #2F3129;color: #8F908A}.ace-monokai .ace_print-margin {width: 1px;background: #555651}.ace-monokai {background-color: #272822;color: #F8F8F2}.ace-monokai .ace_cursor {color: #F8F8F0}.ace-monokai .ace_marker-layer .ace_selection {background: #49483E}.ace-monokai.ace_multiselect .ace_selection.ace_start {box-shadow: 0 0 3px 0px #272822;}.ace-monokai .ace_marker-layer .ace_step {background: rgb(102, 82, 0)}.ace-monokai .ace_marker-layer .ace_bracket {margin: -1px 0 0 -1px;border: 1px solid #49483E}.ace-monokai .ace_marker-layer .ace_active-line {background: #202020}.ace-monokai .ace_gutter-active-line {background-color: #272727}.ace-monokai .ace_marker-layer .ace_selected-word {border: 1px solid #49483E}.ace-monokai .ace_invisible {color: #52524d}.ace-monokai .ace_entity.ace_name.ace_tag,.ace-monokai .ace_keyword,.ace-monokai .ace_meta.ace_tag,.ace-monokai .ace_storage {color: #F92672}.ace-monokai .ace_punctuation,.ace-monokai .ace_punctuation.ace_tag {color: #fff}.ace-monokai .ace_constant.ace_character,.ace-monokai .ace_constant.ace_language,.ace-monokai .ace_constant.ace_numeric,.ace-monokai .ace_constant.ace_other {color: #AE81FF}.ace-monokai .ace_invalid {color: #F8F8F0;background-color: #F92672}.ace-monokai .ace_invalid.ace_deprecated {color: #F8F8F0;background-color: #AE81FF}.ace-monokai .ace_support.ace_constant,.ace-monokai .ace_support.ace_function {color: #66D9EF}.ace-monokai .ace_fold {background-color: #A6E22E;border-color: #F8F8F2}.ace-monokai .ace_storage.ace_type,.ace-monokai .ace_support.ace_class,.ace-monokai .ace_support.ace_type {font-style: italic;color: #66D9EF}.ace-monokai .ace_entity.ace_name.ace_function,.ace-monokai .ace_entity.ace_other,.ace-monokai .ace_entity.ace_other.ace_attribute-name,.ace-monokai .ace_variable {color: #A6E22E}.ace-monokai .ace_variable.ace_parameter {font-style: italic;color: #FD971F}.ace-monokai .ace_string {color: #E6DB74}.ace-monokai .ace_comment {color: #75715E}.ace-monokai .ace_indent-guide {background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAAEklEQVQImWPQ0FD0ZXBzd/wPAAjVAoxeSgNeAAAAAElFTkSuQmCC) right repeat-y}");
-
+ 
+/* Ace Textmate theme.  Taken from https://github.com/ajaxorg/ace/blob/master/lib/ace/theme/textmate.css */
+aceThemes.addTheme("tm", ".ace-tm .ace_gutter {background: #f0f0f0;color: #333;}.ace-tm .ace_print-margin {width: 1px;background: #e8e8e8;}.ace-tm .ace_fold {background-color: #6B72E6;}.ace-tm {background-color: #FFFFFF;color: black;}.ace-tm .ace_cursor {color: black;}.ace-tm .ace_invisible {color: rgb(191, 191, 191);}.ace-tm .ace_storage,.ace-tm .ace_keyword {color: blue;}.ace-tm .ace_constant {color: rgb(197, 6, 11);}.ace-tm .ace_constant.ace_buildin {color: rgb(88, 72, 246);}.ace-tm .ace_constant.ace_language {color: rgb(88, 92, 246);}.ace-tm .ace_constant.ace_library {color: rgb(6, 150, 14);}.ace-tm .ace_invalid {background-color: rgba(255, 0, 0, 0.1);color: red;}.ace-tm .ace_support.ace_function {color: rgb(60, 76, 114);}.ace-tm .ace_support.ace_constant {color: rgb(6, 150, 14);}.ace-tm .ace_support.ace_type,.ace-tm .ace_support.ace_class {color: rgb(109, 121, 222);}.ace-tm .ace_keyword.ace_operator {color: rgb(104, 118, 135);}.ace-tm .ace_string {color: rgb(3, 106, 7);}.ace-tm .ace_comment {color: rgb(76, 136, 107);}.ace-tm .ace_comment.ace_doc {color: rgb(0, 102, 255);}.ace-tm .ace_comment.ace_doc.ace_tag {color: rgb(128, 159, 191);}.ace-tm .ace_constant.ace_numeric {color: rgb(0, 0, 205);}.ace-tm .ace_variable {color: rgb(49, 132, 149);}.ace-tm .ace_xml-pe {color: rgb(104, 104, 91);}.ace-tm .ace_entity.ace_name.ace_function {color: #0000A2;}.ace-tm .ace_heading {color: rgb(12, 7, 255);}.ace-tm .ace_list {color:rgb(185, 6, 144);}.ace-tm .ace_meta.ace_tag {color:rgb(0, 22, 142);}.ace-tm .ace_string.ace_regex {color: rgb(255, 0, 0)}.ace-tm .ace_marker-layer .ace_selection {background: rgb(181, 213, 255);}.ace-tm.ace_multiselect .ace_selection.ace_start {box-shadow: 0 0 3px 0px white;}.ace-tm .ace_marker-layer .ace_step {background: rgb(252, 255, 0);}.ace-tm .ace_marker-layer .ace_stack {background: rgb(164, 229, 101);}.ace-tm .ace_marker-layer .ace_bracket {margin: -1px 0 0 -1px;border: 1px solid rgb(192, 192, 192);}.ace-tm .ace_marker-layer .ace_active-line {background: rgba(0, 0, 0, 0.07);}.ace-tm .ace_gutter-active-line {background-color : #dcdcdc;}.ace-tm .ace_marker-layer .ace_selected-word {background: rgb(250, 250, 255);border: 1px solid rgb(200, 200, 250);}.ace-tm .ace_indent-guide {background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAAE0lEQVQImWP4////f4bLly//BwAmVgd1/w11/gAAAABJRU5ErkJggg==) right repeat-y;}");
+    
 var url = window.location.href.split('/');
 
 var userInfo = {};
@@ -35,12 +38,16 @@ var kaid = KAdefine.require("./javascript/shared-package/ka.js").getKaid(),
 
 var commentLinkGenerator = null;
 
+/** CSS classnames and class prefixes **/
 var extensionCommentClassName = "ka-extension-modified-comment";
 var extensionCommentEditClassName = "ka-extension-modified-comment-edit-link";
 var commentEditClassPrefix = "ka-extension-edit-";
 var commentEditUIClass = "ka-extension-edit-comment-ui-div";
 var commentCancelEditPrefix = "ka-extension-edit-comment-cancel-";
 
+var extensionStoragePrefix = "ka-extension-localstorage-item-";
+var extensionToggleDarkEditorItemKey = extensionStoragePrefix + "toggle-editor-dark-theme";
+    
 function getSession() {
     return /fkey=(.*?);/ig.exec(document.cookie)[1];
 }
@@ -278,9 +285,36 @@ function getProfileData() {
 
 /*** Add editor dark theme ***/
 function darkTheme() {
+    var sid = "ka-extension-ace-override";
+    if(document.getElementById(sid)) { document.body.removeChild(document.getElementById(sid)); }
     var s = document.createElement("style");
-    s.innerHTML = aceThemes.getThemeCSS("monokai");
+    s.id = sid;
+    s.innerHTML = aceThemes.getThemeCSS(+localStorage.getItem(extensionToggleDarkEditorItemKey) ? "monokai" : "tm");
     document.body.appendChild(s);
+}
+    
+function darkToggleButton() {
+    let toolbar = document.getElementsByClassName("toolbar_y6hf3w")[0];
+    if(!toolbar) { return; }
+    let rightArea = toolbar.getElementsByClassName("right_piqaq3")[0];
+    if(!rightArea) { return; }
+    let innerSpan = rightArea.getElementsByTagName("span")[0];
+    if(!innerSpan) { return; }
+    let outerButtonSpan = document.createElement("span");
+    outerButtonSpan.className = "pull-right";
+    let innerButtonLink = document.createElement("a");
+    innerButtonLink.style = "background-color: transparent; color: #FFFFFF; text-decoration: none; -webkit-appearance: none; -moz-appearance: none; border-radius: 4px; cursor: pointer; display: inline-block; font-family: inherit; font-size: 15px; font-weight: 400; margin-right: 3px; padding: 8px 8px; text-align: center; transition: 0.1s all; user-select: none; background: #656565; -webkit-transition: 0.1s all; -webkit-user-select: none;";
+    innerButtonLink.href = "javascript:void(0)";
+    innerButtonLink.textContent = "Toggle Dark Theme";
+    innerButtonLink.addEventListener("mouseover", function() { this.style.background = "#484848"; });
+    innerButtonLink.addEventListener("mouseout", function() { this.style.background = "#656565"; });
+    innerButtonLink.addEventListener("click", function() {
+        localStorage.setItem(extensionToggleDarkEditorItemKey, +!+localStorage.getItem(extensionToggleDarkEditorItemKey));
+        darkTheme();
+    });
+    outerButtonSpan.appendChild(innerButtonLink);
+    innerSpan.insertAdjacentElement("beforeend", outerButtonSpan);
+    clearInterval(addDarkToggleButton);
 }
 
 /*** Add links for all comments when viewing user discussion. ***/
@@ -487,6 +521,7 @@ if (window.location.host === 'www.khanacademy.org') {
     var addEditUIInterval = setInterval(addCommentEditUI, 250);
     if (url[3] === 'computer-programming' || url[3] === 'hour-of-code') {
         darkTheme();
+        var addDarkToggleButton = setInterval(darkToggleButton, 250);
         if(url[4] !== 'new') {
             var addFlags = setInterval(addFlagsToProgram, 250),
                 getDates = setInterval(showProgramDates, 250),
