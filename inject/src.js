5cce620d94fcca990eb5411501b88ef87d508518
Ethan Luis McDonough <elmcdonough@comcast.net>
2018-01-15 19:44:32 -0600

    all right

--- inject/src.js
+++ inject/src.js
@@ -812,7 +812,7 @@ function deleteNotif() {
     if (buttons.length >= notifList.length) return;
 
     for (var i = 0; i < notifList.length; i++) {
-        if(notifList[i].className.length > 0) continue;
+        if(notifList[i].className.length > 0) { continue; }
         var notifElm = notifList[i].childNodes[0];
         if(notifElm === undefined || notifElm.childNodes.length >= 2) continue;
         var notifURL = notifElm.childNodes[0].href;
@@ -821,11 +821,10 @@ function deleteNotif() {
             continue;
         }
         var linkElm = notifElm.childNodes[0];
-        if(linkElm.className === "loadingSpinner_18tyv6y") continue;
+        if(linkElm.className === "loadingSpinner_18tyv6y") { continue; }
 
         var deleteButton = document.createElement("span");
         deleteButton.className = "kae-notif-delete";
-        deleteButton.style = "display: block; margin: 0; padding-left: 5px; padding-top: 5px;"
         deleteButton.innerHTML = `
                 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.9 21.9" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 21.9 21.9" width="10" height="10">
                   <path d="M14.1,11.3c-0.2-0.2-0.2-0.5,0-0.7l7.5-7.5c0.2-0.2,0.3-0.5,0.3-0.7s-0.1-0.5-0.3-0.7l-1.4-1.4C20,0.1,19.7,0,19.5,0  c-0.3,0-0.5,0.1-0.7,0.3l-7.5,7.5c-0.2,0.2-0.5,0.2-0.7,0L3.1,0.3C2.9,0.1,2.6,0,2.4,0S1.9,0.1,1.7,0.3L0.3,1.7C0.1,1.9,0,2.2,0,2.4  s0.1,0.5,0.3,0.7l7.5,7.5c0.2,0.2,0.2,0.5,0,0.7l-7.5,7.5C0.1,19,0,19.3,0,19.5s0.1,0.5,0.3,0.7l1.4,1.4c0.2,0.2,0.5,0.3,0.7,0.3  s0.5-0.1,0.7-0.3l7.5-7.5c0.2-0.2,0.5-0.2,0.7,0l7.5,7.5c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3l1.4-1.4c0.2-0.2,0.3-0.5,0.3-0.7  s-0.1-0.5-0.3-0.7L14.1,11.3z"/>
