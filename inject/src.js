7fd99ad53a190fb4bab3699b9d86f7b3227e6418
Ethan Luis McDonough <elmcdonough@comcast.net>
2018-01-18 11:35:07 -0600

    Tweak notif delete button styling

--- inject/src.js
+++ inject/src.js
@@ -825,13 +825,24 @@ function deleteNotif() {
 
         var deleteButton = document.createElement("span");
         deleteButton.className = "kae-notif-delete";
-        deleteButton.innerHTML = `
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.9 21.9" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 21.9 21.9" width="10" height="10">
-                  <path d="M14.1,11.3c-0.2-0.2-0.2-0.5,0-0.7l7.5-7.5c0.2-0.2,0.3-0.5,0.3-0.7s-0.1-0.5-0.3-0.7l-1.4-1.4C20,0.1,19.7,0,19.5,0  c-0.3,0-0.5,0.1-0.7,0.3l-7.5,7.5c-0.2,0.2-0.5,0.2-0.7,0L3.1,0.3C2.9,0.1,2.6,0,2.4,0S1.9,0.1,1.7,0.3L0.3,1.7C0.1,1.9,0,2.2,0,2.4  s0.1,0.5,0.3,0.7l7.5,7.5c0.2,0.2,0.2,0.5,0,0.7l-7.5,7.5C0.1,19,0,19.3,0,19.5s0.1,0.5,0.3,0.7l1.4,1.4c0.2,0.2,0.5,0.3,0.7,0.3  s0.5-0.1,0.7-0.3l7.5-7.5c0.2-0.2,0.5-0.2,0.7,0l7.5,7.5c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3l1.4-1.4c0.2-0.2,0.3-0.5,0.3-0.7  s-0.1-0.5-0.3-0.7L14.1,11.3z"/>
-                </svg>
-        `
-        deleteButton.addEventListener("click", function(){
-            var href = this.parentNode.childNodes[1].href;
+        
+        let svgNamespace = "http://www.w3.org/2000/svg";
+        
+        let deleteSVG = document.createElementNS(svgNamespace, "svg");
+        deleteSVG.setAttribute("width", "10");
+        deleteSVG.setAttribute("height", "10");
+        deleteSVG.setAttribute("viewBox", "0 0 21.9 21.9");
+        deleteSVG.setAttribute("enable-background", "new 0 0 21.9 21.9");
+
+        let delteSVGPath = document.createElementNS(svgNamespace, "path");
+        delteSVGPath.setAttribute("fill", "#000000");
+        delteSVGPath.setAttribute("d", "M14.1,11.3c-0.2-0.2-0.2-0.5,0-0.7l7.5-7.5c0.2-0.2,0.3-0.5,0.3-0.7s-0.1-0.5-0.3-0.7l-1.4-1.4C20,0.1,19.7,0,19.5,0  c-0.3,0-0.5,0.1-0.7,0.3l-7.5,7.5c-0.2,0.2-0.5,0.2-0.7,0L3.1,0.3C2.9,0.1,2.6,0,2.4,0S1.9,0.1,1.7,0.3L0.3,1.7C0.1,1.9,0,2.2,0,2.4  s0.1,0.5,0.3,0.7l7.5,7.5c0.2,0.2,0.2,0.5,0,0.7l-7.5,7.5C0.1,19,0,19.3,0,19.5s0.1,0.5,0.3,0.7l1.4,1.4c0.2,0.2,0.5,0.3,0.7,0.3  s0.5-0.1,0.7-0.3l7.5-7.5c0.2-0.2,0.5-0.2,0.7,0l7.5,7.5c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3l1.4-1.4c0.2-0.2,0.3-0.5,0.3-0.7  s-0.1-0.5-0.3-0.7L14.1,11.3z");
+        
+        deleteSVG.appendChild(delteSVGPath);
+        deleteButton.appendChild(deleteSVG);
+        
+        deleteSVG.addEventListener("click", function(){
+            var href = this.parentNode.parentNode.childNodes[1].href;
             var notifKey = href.match(/[?&]keys(=([^&#]*)|&|#|$)/)[2].split(",");
             deleted.push(notifKey.join(","));
             for(var i = 0; i < notifKey.length; i++){
