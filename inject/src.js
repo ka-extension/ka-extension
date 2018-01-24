2fec80e308b650c19ff4d473a04b576a5c0366a8
Luke Krikorian <lukekrikorian@gmail.com>
2018-01-24 13:18:52 -0500

    Fixed notification guardian message bug

--- inject/src.js
+++ inject/src.js
@@ -798,6 +798,7 @@ function deleteNotif() {
         var notifElm = notifList[i].childNodes[0];
         if(notifElm === undefined || notifElm.childNodes.length >= 2) continue;
         var notifURL = notifElm.childNodes[0].href;
+        if(notifURL === undefined) return;
         if(deleted.indexOf(notifURL.match(/[?&]keys(=([^&#]*)|&|#|$)/)[2]) > -1){
             notifElm.parentNode.removeChild(notifElm);
             continue;
@@ -807,9 +808,9 @@ function deleteNotif() {
 
         var deleteButton = document.createElement("span");
         deleteButton.className = "kae-notif-delete";
-        
+
         let svgNamespace = "http://www.w3.org/2000/svg";
-        
+
         let deleteSVG = document.createElementNS(svgNamespace, "svg");
         deleteSVG.setAttribute("width", "10");
         deleteSVG.setAttribute("height", "10");
@@ -819,10 +820,10 @@ function deleteNotif() {
         let delteSVGPath = document.createElementNS(svgNamespace, "path");
         delteSVGPath.setAttribute("fill", "#000000");
         delteSVGPath.setAttribute("d", "M14.1,11.3c-0.2-0.2-0.2-0.5,0-0.7l7.5-7.5c0.2-0.2,0.3-0.5,0.3-0.7s-0.1-0.5-0.3-0.7l-1.4-1.4C20,0.1,19.7,0,19.5,0  c-0.3,0-0.5,0.1-0.7,0.3l-7.5,7.5c-0.2,0.2-0.5,0.2-0.7,0L3.1,0.3C2.9,0.1,2.6,0,2.4,0S1.9,0.1,1.7,0.3L0.3,1.7C0.1,1.9,0,2.2,0,2.4  s0.1,0.5,0.3,0.7l7.5,7.5c0.2,0.2,0.2,0.5,0,0.7l-7.5,7.5C0.1,19,0,19.3,0,19.5s0.1,0.5,0.3,0.7l1.4,1.4c0.2,0.2,0.5,0.3,0.7,0.3  s0.5-0.1,0.7-0.3l7.5-7.5c0.2-0.2,0.5-0.2,0.7,0l7.5,7.5c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3l1.4-1.4c0.2-0.2,0.3-0.5,0.3-0.7  s-0.1-0.5-0.3-0.7L14.1,11.3z");
-        
+
         deleteSVG.appendChild(delteSVGPath);
         deleteButton.appendChild(deleteSVG);
-        
+
         deleteSVG.addEventListener("click", function(){
             var href = this.parentNode.parentNode.childNodes[1].href;
             var notifKey = href.match(/[?&]keys(=([^&#]*)|&|#|$)/)[2].split(",");
