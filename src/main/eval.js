1fa50d4f0d2a6120850e4052eb88617cc949dc23
Luke Krikorian <lukekrikorian@gmail.com>
2018-02-27 11:35:03 -0500

    Added check all button for project evals

--- src/main/eval.js
+++ src/main/eval.js
@@ -9,29 +9,27 @@ function evalFeatures() {
     projectInfoNotEmpty.then(projectData => {
         querySelectorPromise(".eval-left > ul").then(list => {
             querySelectorPromise('.edit-content-form__left').then(tips => {
-                console.log(tips);
                 tips.parentNode.removeChild(tips);
-            });
+            }).catch(console.error);
             var textareas = document.getElementsByClassName('eval-text');
-            console.log(textareas);
-            
-            var btn = document.createElement("button");
-            btn.className = "buttonStyle_1quqytj";
-            btn.type = "button";
-            btn.innerText = "Generate Comment";
-            btn.style = "border: 1px solid #ccc !important; margin-left: 1px !important;";
-            btn.addEventListener('click', function(){
-                var objectives = document.getElementsByClassName("eval-peer-rubric-item");
+            var objectives = document.getElementsByClassName("eval-peer-rubric-item");
+
+            var commentBtn = document.createElement("button");
+            commentBtn.className = "buttonStyle_1quqytj";
+            commentBtn.type = "button";
+            commentBtn.innerText = "Generate comment";
+            commentBtn.style = "border: 1px solid #ccc !important; margin-left: 1px !important;";
+            commentBtn.addEventListener('click', function(){
                 if(document.querySelectorAll(".nopass,.pass").length < objectives.length) return;
                 var evalCommentTextarea = textareas[textareas.length - 1];
                 var evalCommentText = "Hey there,\n\n";
                 var addition;
                 var nopass = document.getElementsByClassName("nopass");
                 if(objectives[objectives.length - 1].childNodes[0].className === "nopass"){
-                    addition = comments.plagiarism;    
+                    addition = comments.plagiarism;
                 }
                 else if(nopass.length < 1){
-                    addition = comments.passed; 
+                    addition = comments.passed;
                 }
                 else{
                     addition = comments.needsWork;
@@ -39,7 +37,21 @@ function evalFeatures() {
                 evalCommentText += addition;
                 evalCommentTextarea.value = evalCommentText;
             });
-            list.appendChild(btn);
+
+            var checkBtn = document.createElement("button");
+            checkBtn.className = "buttonStyle_1quqytj";
+            checkBtn.type = "button";
+            checkBtn.innerText = "Check all";
+            checkBtn.style = "border: 1px solid #ccc !important; margin-left: 2px !important; border-radius: 1px !important;";
+            checkBtn.addEventListener("click", function(){
+              for(let objective of objectives){
+                if(objective.querySelector("i").className === "pass") continue;
+                objective.querySelector(".buttonStyle_1quqytj").click();
+              }
+            });
+
+            list.appendChild(commentBtn);
+            list.appendChild(checkBtn);
         }).catch(console.error);
     }).catch(console.error);
 }
